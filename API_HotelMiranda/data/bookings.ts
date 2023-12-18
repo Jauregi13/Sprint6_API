d0fc694dc9406857eedfd63d6c5a95caa4684939
jauregi13 <jon.j1305@gmail.com>
2023-12-18 18:06:46 +0100

    datos añadidos

---
+++ API_HotelMiranda/data/bookings.ts
@@ -0,0 +1,255 @@
+
+
+export const bookings = [
+    {
+    "id": "#100001",
+    "guest": "Juan Pérez",
+    "order_date": "2023-10-01",
+    "check_in": "2023-11-05",
+    "check_out": "2023-11-10",
+    "special_request": "Habitación con vistas al mar",
+    "room_type":  "#1005",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100002",
+    "guest": "María García",
+    "order_date": "2023-10-03",
+    "check_in": "2023-11-08",
+    "check_out": "2023-11-15",
+    "special_request": "Cama extra para niño",
+    "room_type": "#1007",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100003",
+    "guest": "Laura Martínez",
+    "order_date": "2023-10-05",
+    "check_in": "2023-11-10",
+    "check_out": "2023-11-17",
+    "special_request": "Habitación cerca de la piscina",
+    "room_type": "#1008",
+    "status": "In Progress"
+    },
+    {
+    "id": "#100004",
+    "guest": "Carlos Rodríguez",
+    "order_date": "2023-10-08",
+    "check_in": "2023-11-15",
+    "check_out": "2023-11-20",
+    "special_request": "Habitación con balcón",
+    "room_type": "#1012",
+    "status": "Check In"
+    },
+    {
+    "id": "#100005",
+    "guest": "Ana Gómez",
+    "order_date": "2023-10-10",
+    "check_in": "2023-11-20",
+    "check_out": "2023-11-25",
+    "special_request": "Petición de piso alto",
+    "room_type": "#1013",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100006",
+    "guest": "Pedro Sánchez",
+    "order_date": "2023-10-12",
+    "check_in": "2023-11-25",
+    "check_out": "2023-11-30",
+    "special_request": "Habitación con jacuzzi",
+    "room_type": "#1015",
+    "status": "In Progress"
+    },
+    {
+    "id": "#100007",
+    "guest": "Marina López",
+    "order_date": "2023-10-15",
+    "check_in": "2023-12-01",
+    "check_out": "2023-12-06",
+    "special_request": "Petición de habitación tranquila",
+    "room_type": "#1017",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100008",
+    "guest": "Javier Martínez",
+    "order_date": "2023-10-18",
+    "check_in": "2023-12-05",
+    "check_out": "2023-12-10",
+    "special_request": "Habitación con vistas a la ciudad",
+    "room_type": "#1018",
+    "status": "Check In"
+    },
+    {
+    "id": "#100009",
+    "guest": "Sofía García",
+    "order_date": "2023-10-20",
+    "check_in": "2023-12-10",
+    "check_out": "2023-12-15",
+    "special_request": "Petición de cama king-size",
+    "room_type": "#1017",
+    "status": "In Progress"
+    },
+    {
+    "id": "#100010",
+    "guest": "Diego Pérez",
+    "order_date": "2023-10-22",
+    "check_in": "2023-12-15",
+    "check_out": "2023-12-20",
+    "special_request": "Habitación adaptada para personas con movilidad reducida",
+    "room_type": "#1017",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100011",
+    "guest": "Lucía Rodríguez",
+    "order_date": "2023-10-25",
+    "check_in": "2023-12-20",
+    "check_out": "2023-12-25",
+    "special_request": "Petición de desayuno en la habitación",
+    "room_type": "#1017",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100012",
+    "guest": "Martín Gómez",
+    "order_date": "2023-10-28",
+    "check_in": "2023-12-25",
+    "check_out": "2023-12-30",
+    "special_request": "Habitación con terraza",
+    "room_type": "#1010",
+    "status": "In Progress"
+    },
+    {
+    "id": "#100013",
+    "guest": "Valeria Sánchez",
+    "order_date": "2023-10-30",
+    "check_in": "2023-12-30",
+    "check_out": "2024-01-04",
+    "special_request": "Petición de habitación silenciosa",
+    "room_type": "#1022",
+    "status": "Check In"
+    },
+    {
+    "id": "#100014",
+    "guest": "Luisa López",
+    "order_date": "2023-11-02",
+    "check_in": "2024-01-05",
+    "check_out": "2024-01-10",
+    "special_request": "Habitación con vistas al jardín",
+    "room_type": "#1023",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100015",
+    "guest": "Gabriel Martínez",
+    "order_date": "2023-11-05",
+    "check_in": "2024-01-10",
+    "check_out": "2024-01-15",
+    "special_request": "Petición de late check-out",
+    "room_type": "#1025",
+    "status": "In Progress"
+    },
+    {
+    "id": "#100016",
+    "guest": "Carolina García",
+    "order_date": "2023-11-08",
+    "check_in": "2024-01-15",
+    "check_out": "2024-01-20",
+    "special_request": "Habitación con minibar",
+    "room_type": "#1024",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100017",
+    "guest": "Andrés Pérez",
+    "order_date": "2023-11-10",
+    "check_in": "2024-01-20",
+    "check_out": "2024-01-25",
+    "special_request": "Petición de albornoces en la habitación",
+    "room_type": "#1026",
+    "status": "Check In"
+    },
+    {
+    "id": "#100018",
+    "guest": "Camila Rodríguez",
+    "order_date": "2023-11-12",
+    "check_in": "2024-01-25",
+    "check_out": "2024-01-30",
+    "special_request": "Habitación con acceso directo a la piscina",
+    "room_type": "#1028",
+    "status": "In Progress"
+    },
+    {
+    "id": "#100019",
+    "guest": "Emilio Gómez",
+    "order_date": "2023-11-15",
+    "check_in": "2024-01-30",
+    "check_out": "2024-02-04",
+    "special_request": "Petición de habitación con escritorio",
+    "room_type": "#1030",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100020",
+    "guest": "Sara Sánchez",
+    "order_date": "2023-11-18",
+    "check_in": "2024-02-05",
+    "check_out": "2024-02-10",
+    "special_request": "Habitación con bañera de hidromasaje",
+    "room_type": "#1029",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100021",
+    "guest": "Daniel López",
+    "order_date": "2023-11-20",
+    "check_in": "2024-02-10",
+    "check_out": "2024-02-15",
+    "special_request": "Petición de habitación con vistas panorámicas",
+    "room_type": "#1028",
+    "status": "In Progress"
+    },
+    {
+    "id": "#100022",
+    "guest": "Isabella Martínez",
+    "order_date": "2023-11-22",
+    "check_in": "2024-02-15",
+    "check_out": "2024-02-20",
+    "special_request": "Habitación con servicio de habitaciones las 24 horas",
+    "room_type": "#1003",
+    "status": "Check Out"
+    },
+    {
+    "id": "#100023",
+    "guest": "Marcos García",
+    "order_date": "2023-11-25",
+    "check_in": "2024-02-20",
+    "check_out": "2024-02-25",
+    "special_request": "Petición de habitación con cocina",
+    "room_type": "#1005",
+    "status": "Check In"
+    },
+    {
+    "id": "#100024",
+    "guest": "Valentina Pérez",
+    "order_date": "2023-11-28",
+    "check_in": "2024-02-25",
+    "check_out": "2024-02-29",
+    "special_request": "Habitación con acceso a terraza privada",
+    "room_type": "#1008",
+    "status": "In Progress"
+    },
+    {
+    "id": "#100025",
+    "guest": "Lucas Rodríguez",
+    "order_date": "2023-11-30",
+    "check_in": "2024-02-29",
+    "check_out": "2024-03-04",
+    "special_request": "Petición de habitación tranquila",
+    "room_type": "#1013",
+    "status": "Check Out"
+    }
+]
+  

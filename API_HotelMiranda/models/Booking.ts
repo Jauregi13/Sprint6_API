1fc4fe4176444d0609fe456198dfb04887f3e5e7
jauregi13 <jon.j1305@gmail.com>
2023-12-18 17:40:00 +0100

    modelos añadidos

---
+++ API_HotelMiranda/models/Booking.ts
@@ -0,0 +1,15 @@
+
+
+
+export interface BookingInterface {
+
+    id: string
+    guest: string
+    order_date: Date
+    check_in: Date
+    check_out: Date
+    special_request: string
+    room_type: string
+    status: string
+
+}
---
+++ API_HotelMiranda/models/Room.ts
@@ -0,0 +1,15 @@
+
+
+export interface RoomInterface {
+
+    id: string
+    room_type: string
+    room_number: string
+    description: string
+    amenities: string[]
+    cancellation: string
+    price: number
+    offer: number
+    status: string
+
+}

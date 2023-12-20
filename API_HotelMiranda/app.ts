743294b4b78865a570509980058d8e1075ce4601
jauregi13 <jon.j1305@gmail.com>
2023-12-20 14:35:01 +0100

    ruta publica añadida

--- API_HotelMiranda/app.ts
+++ API_HotelMiranda/app.ts
@@ -7,17 +7,25 @@ import { NextFunction, Request, Response } from 'express'
 import { authenticateToken } from './services/login'
 
 const express = require('express')
+const listEndpoints = require('express-list-endpoints')
 export const app = express()
 app.use(express.json());
 
 app.use('/', loginRouter)
 
+app.get('/',(req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.json({nameHotel: 'Hotel Miranda API', endpoints: listEndpoints(app)})
+})
+
 app.use(authenticateToken)
 
-app.use('/',roomRouter)
+app.use('/', roomRouter)
 app.use('/', bookingRouter)
 app.use('/',userRouter)
 app.use('/',contactRouter)
+
+
 
 
 

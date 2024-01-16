69bb83210d94fc38ce7fff502b70d9fe9a945953
jauregi13 <jon.j1305@gmail.com>
2024-01-16 20:40:36 +0100

    conexión actualizada a MySQL

---
+++ connectionMySQL.ts
@@ -0,0 +1,26 @@
+
+import mysql from 'mysql2/promise'
+const dotenv = require('dotenv')
+dotenv.config({ path: `.env.local`, override: true })
+
+export const connectionMySQL = async () => {
+
+
+    try {
+
+        await mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: process.env.PASSWORD_MYSQL,
+            database: 'hotelmiranda'
+        })
+        
+        console.log('connected to DB');
+        
+
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
